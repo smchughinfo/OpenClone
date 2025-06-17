62b0f24f5f1a19046531418d89d91ad6ab4f0580
smchughinfo <seanmchugh.info@gmail.com>
2025-06-17 14:00:29 -0400

    Fix LogWeaver version

--- LogWeaver/setup.py
+++ LogWeaver/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="openclone-logweaver",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     url='https://github.com/smchughinfo/LogWeaver',
     install_requires=[
