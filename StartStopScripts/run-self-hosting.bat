db1eea8530de462d8bbb489b4548c8da93a2143d
smchughinfo <seanmchugh.info@gmail.com>
2025-08-02 14:39:44 -0400

    add self-hosting batch script

--- StartStopScripts/run-self-hosting.bat
+++ StartStopScripts/run-self-hosting.bat
@@ -9,17 +9,9 @@ rem START LOGVIEWER
 call LogViewer\stop.bat
 call LogViewer\start-local.bat
 
-rem START WEBPACK
-call WebPack\stop.bat
-call WebPack\start.bat
-
 rem START SADTALKER
 call SadTalker\stop.bat
 call SadTalker\start.bat
-
-rem START U-2-NET
-rem call U-2-Net\stop.bat
-rem call U-2-Net\start.bat
 
 rem START WEBSITE
 call Website\start.bat
